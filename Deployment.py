85e7d91386ced812df1fc251773efdfda522c8bb
Ahmed-Nezar <ahmednezarrr@gmail.com>
2024-05-16 02:34:09 +0300

    fix: cDCGAN Gray Model

--- Deployment.py
+++ Deployment.py
@@ -62,7 +62,7 @@ path_cGAN_Colored_128 = 'Trained-Models\\GAN_Colored_128_Gen_750.pth'
 # DCGANs Flipped
 #######################################
 path_cDCGAN_Colored_Flip_64 = 'Trained-Models\\CDCGAN_Colored_64_Flipped_Gen_500.pth'
-path_cDCGAN_Gray_Flip_128 = 'Trained-Models\\CDCGANs_Gray_Scale_128_500.pth'
+path_cDCGAN_Gray_Flip_128 = 'Trained-Models\CDC_gen_450.pth'
 
 
 # WGANs Gray
@@ -122,7 +122,11 @@ gen_cDCGAN_Colored_Flip_64_Model = cDCGAN_Generator_Colored_64_Flip(
 gen_cDCGAN_Colored_Flip_64_Model.load_state_dict(
     gen_cDCGAN_Colored_Flip_64)
 gen_cDCGAN_Colored_Flip_64_Model.eval()
-# gen_cDCGAN_Gray_Flip_128 = cDCGAN_Generator_Gray_128_Flip(100, 3, 64, 3, 128, 100).load_state_dict(gen_cDCGAN_Gray_Flip_128) -> error
+
+gen_cDCGAN_Gray_Flip_128_Model = cDCGAN_Generator_Gray_128_Flip(100, 3, 64, 3, 128, 100)
+gen_cDCGAN_Gray_Flip_128_Model.load_state_dict(gen_cDCGAN_Gray_Flip_128) 
+gen_cDCGAN_Gray_Flip_128_Model.eval()
+
 
 gen_WGAN_Gray_Flip_64_Model = cWGAN_Generator_64(
     100, 3, 64, 3, 64, 100)
@@ -275,8 +279,8 @@ def main_page(submitted: bool, num_outputs: int,
 
                 if refine == "cDCGAN-64-C":
                     model = gen_cDCGAN_Colored_Flip_64_Model
-                # elif refine == "cDCGAN-128-NC":
-                #     model = gen_cDCGAN_Gray_Flip_128_Model
+                elif refine == "cDCGAN-128-NC":
+                    model = gen_cDCGAN_Gray_Flip_128_Model
                 elif refine == "CWGAN-64-F-NC":
                     model = gen_WGAN_Gray_Flip_64_Model
                 elif refine == "CWGAN-64-F-C":
